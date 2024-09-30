e59f75fdda59326029e5d1dd3db073361d29c983
Ian <iancunha20@gmail.com>
2024-09-29 21:13:56 -0300

    Update Container.tsx

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -4,7 +4,7 @@ function Container() {
       <div className="hero-flex">
         <h1 className="slogan-hero">Alcance bons <br />resultados.</h1>
         <h2 className="frase-hero">Criamos o seu site e construímos uma identidade visual única do seu negócio.</h2>
-        <a className="btn-hero" target="_blank" href=""> QUERO UM ORÇAMENTO <i className="fs-1 bi bi-arrow-right-circle-fill"></i></a>
+        <a className="btn-hero" target="_blank" href="https://wa.me/5581993964043"> QUERO UM ORÇAMENTO <i className="fs-1 bi bi-arrow-right-circle-fill"></i></a>
       </div>
       <div className="hero-flex">
         <i className="bi bi-code-slash"></i>
