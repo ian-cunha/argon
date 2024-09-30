9165a513e990c39364a9ef5434dd80d8b5ff6eb5
Ian <iancunha20@gmail.com>
2024-09-29 21:18:03 -0300

    update

--- src/components/Contato.tsx
+++ src/components/Contato.tsx
@@ -1,7 +1,7 @@
 function Contato() {
     return (
         <>
-            <div className="contact-section" id="contato">
+            <div className="contact-section" id='contato'>
                 <h2 className="contact-title">Entre em Contato</h2>
                 <div className="contact-methods">
                     <a className="contact-link" href="https://wa.me/5581993964043" target="_blank" rel="noopener noreferrer">
