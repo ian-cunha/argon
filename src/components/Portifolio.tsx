b4fa97e040ec552f889e88c4a5a2af2be0c428f0
Ian <iancunha20@gmail.com>
2024-10-06 12:01:28 -0300

    update - izzi.paper

--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -3,29 +3,29 @@ function Portifolio() {
         <>
             <div className="portfolio-section" id='portifolio'>
                 <h2 className="portfolio-title">Portfólio</h2>
-                <p>Em processo de desenvolvimento...</p>
-                {/*
                 <div className="portfolio-grid">
                     <div className="portfolio-item">
-                        <img src="link-para-imagem1.jpg" alt="Projeto 1" />
-                        <h3>Projeto 1</h3>
-                        <p>Descrição do projeto 1.</p>
+                        <img src="https://www.izzipaper.shop/assets/logo-D4ltdFPI.png" alt="izzi.paper" />
+                        <h3>izzi.paper</h3>
+                        <p>Loja de produtos de palelaria, chaveiros e mangás.</p>
+                        <a className="btn-port" href="https://www.izzipaper.shop" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
                     </div>
                     <div className="portfolio-item">
                         <img src="link-para-imagem2.jpg" alt="Projeto 2" />
                         <h3>Projeto 2</h3>
                         <p>Descrição do projeto 2.</p>
+                        <a className="btn-port" href="#" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
                     </div>
                     <div className="portfolio-item">
                         <img src="link-para-imagem3.jpg" alt="Projeto 3" />
                         <h3>Projeto 3</h3>
                         <p>Descrição do projeto 3.</p>
+                        <a className="btn-port" href="#" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
                     </div>
                 </div>
-                */}
             </div>
         </>
-    )
+    );
 }
 
-export default Portifolio
+export default Portifolio;
