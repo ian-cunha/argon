9377a3e674185241fea0f729300e09e49ff0ab61
Ian <iancunha20@gmail.com>
2024-10-10 22:46:42 -0300

    Update Portifolio.tsx

--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -12,9 +12,9 @@ function Portifolio() {
                     </div>
                     <div className="portfolio-item">
                         <img src="link-para-imagem2.jpg" alt="Projeto 2" />
-                        <h3>Projeto 2</h3>
-                        <p>Descrição do projeto 2.</p>
-                        <a className="btn-port" href="#" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
+                        <h3>Docklink</h3>
+                        <p>Docklink é um aplicativo que permite criar e compartilhar links personalizados com seus contatos, redes sociais e plataformas de mídia.</p>
+                        <a className="btn-port" href="https://docklink.vercel.app" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
                     </div>
                     <div className="portfolio-item">
                         <img src="link-para-imagem3.jpg" alt="Projeto 3" />
