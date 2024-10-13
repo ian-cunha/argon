7b4165bed12e05b9bb14cb701257c2d48bc5dd31
Ian <iancunha20@gmail.com>
2024-10-13 07:26:03 -0300

    Update Portifolio.tsx

--- src/components/Portifolio.tsx
+++ src/components/Portifolio.tsx
@@ -13,14 +13,8 @@ function Portifolio() {
                     <div className="portfolio-item">
                         <img src="https://www.emporiumdojogador.online/assets/logo-ND8VWH2Y.svg" alt="Emporium do jogador" />
                         <h3>Emporium do jogador</h3>
-                        <p>Embarque em suas aventuras de Dungeons & Dragons com o Emporium do Jogador e descubra um mundo de raças, classes e magias. Seja o mestre do seu destino e deixe que o Emporium do Jogador ilumine seu caminho através dos reinos mágicos e perigosos de D&D. Que as rolagens de dados estejam sempre a seu favor!</p>
+                        <p>Plataforma de rpg de mesa, criador de ficha, dado virtual, gerenciador de personagem e muito mais!</p>
                         <a className="btn-port" href="https://www.emporiumdojogador.online" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
-                    </div>
-                    <div className="portfolio-item">
-                        <img src="link-para-imagem3.jpg" alt="Projeto 3" />
-                        <h3>Projeto 3</h3>
-                        <p>Descrição do projeto 3.</p>
-                        <a className="btn-port" href="#" target="_blank" rel="noopener noreferrer">Ver Projeto</a>
                     </div>
                 </div>
             </div>
